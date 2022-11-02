4845231f22ab1cf93934b82eee6afb8d6868ab24
focus zheng <zffocus@gmail.com>
2022-11-02 10:48:34 +0800

    feature: use pvcscheduler only

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -22,15 +22,7 @@ import (
 	"k8s.io/component-base/cli"
 	"k8s.io/kubernetes/cmd/kube-scheduler/app"
 
-	"sigs.k8s.io/scheduler-plugins/pkg/capacityscheduling"
-	"sigs.k8s.io/scheduler-plugins/pkg/coscheduling"
-	"sigs.k8s.io/scheduler-plugins/pkg/noderesources"
-	"sigs.k8s.io/scheduler-plugins/pkg/noderesourcetopology"
-	"sigs.k8s.io/scheduler-plugins/pkg/podstate"
-	"sigs.k8s.io/scheduler-plugins/pkg/preemptiontoleration"
-	"sigs.k8s.io/scheduler-plugins/pkg/qos"
-	"sigs.k8s.io/scheduler-plugins/pkg/trimaran/loadvariationriskbalancing"
-	"sigs.k8s.io/scheduler-plugins/pkg/trimaran/targetloadpacking"
+	"sigs.k8s.io/scheduler-plugins/pkg/pvcscheduler"
 
 	// Ensure scheme package is initialized.
 	_ "sigs.k8s.io/scheduler-plugins/apis/config/scheme"
@@ -41,17 +33,7 @@ func main() {
 	// Later they can consist of scheduler profile(s) and hence
 	// used by various kinds of workloads.
 	command := app.NewSchedulerCommand(
-		app.WithPlugin(capacityscheduling.Name, capacityscheduling.New),
-		app.WithPlugin(coscheduling.Name, coscheduling.New),
-		app.WithPlugin(loadvariationriskbalancing.Name, loadvariationriskbalancing.New),
-		app.WithPlugin(noderesources.AllocatableName, noderesources.NewAllocatable),
-		app.WithPlugin(noderesourcetopology.Name, noderesourcetopology.New),
-		app.WithPlugin(preemptiontoleration.Name, preemptiontoleration.New),
-		app.WithPlugin(targetloadpacking.Name, targetloadpacking.New),
-		// Sample plugins below.
-		// app.WithPlugin(crossnodepreemption.Name, crossnodepreemption.New),
-		app.WithPlugin(podstate.Name, podstate.New),
-		app.WithPlugin(qos.Name, qos.New),
+		app.WithPlugin(pvcscheduler.Name, pvcscheduler.New),
 	)
 
 	code := cli.Run(command)
